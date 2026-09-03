ce874f343a8535a8f3bdb120af8334f1fd7a5e55
mardo <mfn21@inf.ufpr.br>
2026-09-03 14:29:46 -0300

    novos kernek filters

--- TA01/filter.py
+++ TA01/filter.py
@@ -26,6 +26,18 @@ kernelH = np.array([
     [-1, -2, -1]
 ], dtype=np.float32)
 
+kernelH5 = np.array([
+    [1, 2, 1, 2, 1],
+    [0, 0, 0, 0, 0],
+    [-1, -2, -1, -2, -1]
+], dtype=np.float32)
+
+kernelH11 = np.array([
+    [1, 2, 2, 1, 1, 1, 1, 1, 2, 2, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0],
+    [-1, -2, -2, -1, -1, -1, -1, -1, -2, -2, -1]
+], dtype=np.float32)
+
 
 # kernel Vertical
 kernelV = np.array([
@@ -48,6 +60,12 @@ kernelD135 = np.array([
     [0, 1, 2]
 ], dtype=np.float32)
 
+kernelC = np.array([
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, -1, 0]
+], dtype=np.float32)
+
 #aplicando filtro
 responseV = cv2.filter2D(
     imagemCinza,
@@ -56,7 +74,6 @@ responseV = cv2.filter2D(
 )
 
 # circular
-kernelC = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
 
 responseH = cv2.filter2D(
     imagemCinza,
@@ -64,6 +81,18 @@ responseH = cv2.filter2D(
     kernelH
 )
 
+
+responseH5 = cv2.filter2D(
+    imagemCinza,
+    cv2.CV_32F,
+    kernelH5
+)
+
+responseH11 = cv2.filter2D(
+    imagemCinza,
+    cv2.CV_32F,
+    kernelH11
+)
 
 responseD45 = cv2.filter2D(
     imagemCinza,
@@ -83,12 +112,72 @@ responseC = cv2.filter2D(
     kernelC
 )
 
+#normalização das imagens
+# pode printar as imagens direto com os reponses acima, porém fica feio de ver
+# aqui normalizamos 
+magnitudeV = np.abs(responseV)
+visV = cv2.normalize(
+    magnitudeV,
+    None,
+    0,
+    255,
+    cv2.NORM_MINMAX
+).astype(np.uint8)
 
-# isso daqui instancia a imagem
-cv2.imshow('Teste Vertical', responseV)
-cv2.imshow('Teste Horizontal', responseH)
+magnitudeH = np.abs(responseH)
+visH = cv2.normalize(
+    magnitudeV,
+    None,
+    0,
+    255,
+    cv2.NORM_MINMAX
+).astype(np.uint8)
+
+magnitudeD45 = np.abs(responseD45)
+visD45 = cv2.normalize(
+    magnitudeV,
+    None,
+    0,
+    255,
+    cv2.NORM_MINMAX
+).astype(np.uint8)
+
+magnitudeD135 = np.abs(responseD135)
+visD135 = cv2.normalize(
+    magnitudeV,
+    None,
+    0,
+    255,
+    cv2.NORM_MINMAX
+).astype(np.uint8)
+
+magnitudeC = np.abs(responseC)
+visC = cv2.normalize(
+    magnitudeV,
+    None,
+    0,
+    255,
+    cv2.NORM_MINMAX
+).astype(np.uint8)
+
+
+# printar imagens normalizadas, sem valores negativos de pixel
+# o filtrosdo cv2 pode retorna vlores do tipo: -500 à 500
+# normalizar deixa os pixel no range: 0 à 255 apenas
+# cv2.imshow("Teste Vertical2", visV)
+# cv2.imshow("Teste Horizontal", visH)
+# cv2.imshow("Teste Diagonal 45", visD45)
+# cv2.imshow("Teste Diagonal 135", visD135)
+# cv2.imshow("Teste Circular", visC)
+
+
+# printar imagens sem normalização
+cv2.imshow('Teste Vertical', responseH)
+# cv2.imshow('Teste Vertical escala 5x5', responseH5)
+# cv2.imshow('Teste Vertical escala 11x11', responseH11)
+cv2.imshow('Teste Horizontal', responseV)
 cv2.imshow('Teste diagonal 45 graus', responseD45)
-# cv2.imshow('Teste diagonal 135 graus', responseD135)
+cv2.imshow('Teste diagonal 135 graus', responseD135)
 cv2.imshow('Teste circular', responseC)
 cv2.waitKey(0)  # sem isso, ela abre e fecha rapidamente
 cv2.destroyAllWindows()
