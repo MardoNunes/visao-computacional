38c897af75661c4e78a7da89db0c565ff8b804fe
mardo <mfn21@inf.ufpr.br>
2026-09-03 11:13:49 -0300

    first kernel

--- TA01/filter.py
+++ TA01/filter.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Lê uma imagem do disco (formato padrão BGR)
 imagem = cv2.imread("ufpr.jpg")
@@ -18,7 +19,21 @@ imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 tamanho = (512, 512)
 imagemCinza = cv2.resize(imagemCinza, tamanho, interpolation=cv2.INTER_LINEAR) # redimensiona o tamanho da imagem
 
+# kernel horizontal?
+kernel = np.array([
+    [-1, 0, 1],
+    [-1, 0, 1],
+    [-1, 0, 1]
+], dtype=np.float32)
+
+#aplicando filtro
+response = cv2.filter2D(
+    imagemCinza,
+    cv2.CV_32F,
+    kernel
+)
+
 # isso daqui instancia a imagem
-cv2.imshow('Teste', imagemCinza)
+cv2.imshow('Teste', response)
 cv2.waitKey(0)  # sem isso, ela abre e fecha rapidamente
 cv2.destroyAllWindows()
